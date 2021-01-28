d16d64a8c5b21e902e83bda58c704b7da862fdcb
Damiano <s1lentp4nther@gmail.com>
2021-01-28 11:56:10 +0100

    bbzw sursee Aufgabe

---
+++ Aufgabe01 M404/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Aufgabe01_M404
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxEingabe = new System.Windows.Forms.TextBox();
+            this.textBoxAusgabe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verdopple";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.VerdoppleEvent);
+            // 
+            // textBoxEingabe
+            // 
+            this.textBoxEingabe.Location = new System.Drawing.Point(26, 130);
+            this.textBoxEingabe.Name = "textBoxEingabe";
+            this.textBoxEingabe.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEingabe.TabIndex = 1;
+            // 
+            // textBoxAusgabe
+            // 
+            this.textBoxAusgabe.Location = new System.Drawing.Point(238, 130);
+            this.textBoxAusgabe.Name = "textBoxAusgabe";
+            this.textBoxAusgabe.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAusgabe.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eingabe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ausgabe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 258);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAusgabe);
+            this.Controls.Add(this.textBoxEingabe);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxEingabe;
+        private System.Windows.Forms.TextBox textBoxAusgabe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Aufgabe01 M404/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe01_M404
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void VerdoppleEvent(object sender, EventArgs e)
+        {
+            int iEingabe, iAusgabe;
+
+            int.TryParse(textBoxEingabe.Text, out iEingabe);
+            int.TryParse(textBoxAusgabe.Text, out iAusgabe);
+
+            iAusgabe = iEingabe * 2;
+
+            textBoxAusgabe.Text = Convert.ToString(iAusgabe);
+        }
+    }
+}
